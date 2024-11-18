31e9361970786ec7c779eeed1395a70a012025b9
Aman Agarwal <agarwalaman190202@gmail.com>
2024-11-18 16:01:18 -0500

    Neural Network added

---
+++ main.py
@@ -0,0 +1,55 @@
+from configargparse import ArguementParser
+from load_llff import load_llff_data
+import numpy as np
+
+def train():
+    parser = ArguementParser()
+    parser.add_argument('--config', is_config_file=True, 
+                        help='config file path')
+    
+    args = parser.parse_args()
+
+    # Only meant for LLFF scenes
+    if args.dataset_type == 'llff':
+
+        # Let us not touch the dataloaders, these basically recall all the neccessary data from the files into our program
+        images, poses, bds, render_poses, i_test = load_llff_data(args.datadir, args.factor, # factor tells us if we have to sample down the images, which we should if our resolution is too high
+                                                                  recenter=True, bd_factor=.75,
+                                                                  spherify=args.spherify)
+
+        H, W, focal = poses[0,:3,-1] # height width focal length
+        K = np.array([
+            [focal, 0, 0.5*W],
+            [0, focal, 0.5*H],
+            [0, 0, 1]
+        ])
+        poses = poses[:,:3,:4] # exrtrinsic matrix paramters
+
+        print('Loaded llff', images.shape, render_poses.shape)
+
+        if not isinstance(i_test, list):
+            i_test = [i_test]
+
+        if args.llffhold > 0:  # if args.llffhold = 5, we use every 5th image as our test image
+            print('Auto LLFF holdout,', args.llffhold)
+            i_test = np.arange(images.shape[0])[::args.llffhold]
+
+        # near and far bounding planes of the rendering space
+        print('DEFINING BOUNDS') 
+        if args.no_ndc:
+            near = np.ndarray.min(bds) * .9
+            far = np.ndarray.max(bds) * 1.
+
+        else:
+            near = 0.
+            far = 1.
+
+        print('NEAR FAR', near, far)
+
+
+
+    pass
+
+
+if __name__=='__main__':
+    train()
---
+++ nerf.py
@@ -0,0 +1,47 @@
+import torch
+from torch import nn
+import torch.nn.functional as F
+
+class NeRF(nn.Module):
+    def __init__(self, D=8, W=256, input_ch=3, input_ch_views=3, output_ch=4, skips=[4], use_viewdirs=False):
+
+        super(NeRF, self).__init__()
+        self.D = D
+        self.W = W
+        self.input_ch = input_ch
+        self.input_ch_views = input_ch_views
+        self.skips = skips
+
+        
+        self.pts_linears = nn.ModuleList(
+            [nn.Linear(input_ch, W)] + [nn.Linear(W, W) if i not in self.skips else nn.Linear(W + input_ch, W) for i in range(D-1)])
+        
+        ### Implementation according to the official code release (https://github.com/bmild/nerf/blob/master/run_nerf_helpers.py#L104-L105)
+        self.views_linears = nn.ModuleList([nn.Linear(input_ch_views + W, W//2)])
+        
+        self.feature_linear = nn.Linear(W, W)
+        self.alpha_linear = nn.Linear(W, 1)
+        self.rgb_linear = nn.Linear(W//2, 3)
+
+
+    def forward(self, x):
+        input_pts, input_views = torch.split(x, [self.input_ch, self.input_ch_views], dim=-1)
+        h = input_pts
+        for i, l in enumerate(self.pts_linears):
+            h = self.pts_linears[i](h)
+            h = F.relu(h)
+            if i in self.skips:
+                h = torch.cat([input_pts, h], -1)
+
+        alpha = self.alpha_linear(h)
+        feature = self.feature_linear(h)
+        h = torch.cat([feature, input_views], -1)
+    
+        for i, _ in enumerate(self.views_linears):
+            h = self.views_linears[i](h)
+            h = F.relu(h)
+
+        rgb = self.rgb_linear(h)
+        outputs = torch.cat([rgb, alpha], -1)
+
+        return outputs  
